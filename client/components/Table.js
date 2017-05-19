7573a99a9bc48ffcbd7736c10ea7583ab300c448
Joshua Wolters <joshuawolters@Joshuas-MacBook-Pro.local>
2017-05-19 13:30:32 -0700

    create hard coded node graph

--- client/components/Table.js
+++ client/components/Table.js
@@ -24,21 +24,52 @@ class Table extends Component {
     let holderStyle3={
       "width":"75%",
       "height":"100%",
-      "float":"left"
+      "float":"left",
+      "flex-direction":"column"
     }
     let holderStyle4={
       "width":"100%",
       "height":"50%",
+      "clear":"both",
+    }
+    let holderStyle5={
+      "width":"40%",
+      "height":"100%",
       "float":"left"
     }
+    let holderStyle6={
+      "width":"100%",
+      "height":"100%",
+      "float":"left",
+      "flex-direction":"column"
+    }
+
 
 
     return(
       <div className="holder" style={holderStyle}>
-        <div className="holder" style={holderStyle2}></div>
+        <div className="holder" style={holderStyle2}><div className ="content">Node 1</div></div>
         <div className="holder" style={holderStyle3}>
-          <div className="holder" style={holderStyle4}></div>
-          <div className="holder" style={holderStyle4}></div>
+          <div className="holder" style={holderStyle4}>
+            <div className="holder" style={holderStyle5}><div className ="content">Node 2</div></div>
+            <div className = "holder" style={holderStyle6}>
+              <div className="holder" style={holderStyle4}>
+                <div className = "holder" style={holderStyle5}><div className ="content">Node 4</div></div>
+                <div className = "holder" style={holderStyle}><div className ="content">Node 6</div></div>
+              </div>
+              <div className="holder" style={holderStyle4}>
+                <div className = "holder" style={holderStyle5}><div className ="content">Node 5</div></div>
+                <div className = "holder" style={holderStyle6}>
+                  <div className = "holder" style={holderStyle4}><div className ="content">Node 7</div></div>
+                  <div className = "holder" style={holderStyle4}><div className ="content">Node 8</div></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="holder" style={holderStyle4}>
+            <div className = "holder" style={holderStyle5}><div className ="content">Node 3</div></div>
+            <div className = "holder" style={holderStyle}><div className ="content">Node 9</div></div>
+          </div>
         </div>
       </div>
     )
